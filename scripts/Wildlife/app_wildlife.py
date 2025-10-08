256894056191c85c125b8ec3e92637d712bd43af
AbinadabeFont <abimnegofontenele@gmail.com>
2025-10-08 19:38:24 -0400

    Adicionando rodape e subtitulos aos graficos

--- scripts/Wildlife/app_wildlife.py
+++ scripts/Wildlife/app_wildlife.py
@@ -226,6 +226,7 @@ sys.exit()
 
 #Rodando Dispersao Geral dos animais: Tangara e Jaguar
 run_all_dispersion()
+sys.exit()
 
 for current_animal in list_animals:
     run_kmeans(current_animal, file_rawdata)
