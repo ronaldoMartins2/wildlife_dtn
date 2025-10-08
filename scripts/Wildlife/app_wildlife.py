47b9b3be8d63a199073e29f6e6bfb74e2d03c84a
AbinadabeFont <abimnegofontenele@gmail.com>
2025-10-08 18:25:49 -0400

    Graficos de dispersao todos os animais concluidos

--- scripts/Wildlife/app_wildlife.py
+++ scripts/Wildlife/app_wildlife.py
@@ -102,7 +102,7 @@ from Clusterization.plot_dispersion import (
 )
 
 from Clusterization.plot_dispersion_geral import (
-    run_geral as run_dispersion_geral
+    run_all_dispersion as run_all_dispersion
 )
 
 from DTN.mobility_contacts import (
@@ -224,17 +224,10 @@ sys.exit()
 ############## CLUSTERIZATION ##############################
 # run clusterization kmeans
 
-#for current_animal in list_animals:
-    #print(f"--- Gerando Gráfico de Dispersão para o animal {current_animal} ---")
-    #run_dispersion_plot(current_animal, file_rawdata)
-
-run_dispersion_geral()
-sys.exit()
+#Rodando Dispersao Geral dos animais: Tangara e Jaguar
+run_all_dispersion()
 
 for current_animal in list_animals:
-    print(f"--- Gerando Gráfico de Dispersão para o animal {current_animal} ---")
-    run_dispersion_plot(current_animal, file_rawdata)
-
     run_kmeans(current_animal, file_rawdata)
     run_som(current_animal, file_rawdata)
     run_mean_shift(current_animal, file_rawdata)
